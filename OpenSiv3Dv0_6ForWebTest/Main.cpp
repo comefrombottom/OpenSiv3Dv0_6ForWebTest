7ecff6fc1aa8b020cefaf35956ddb1947f8b71f6
comefrombottom <120954679+comefrombottom@users.noreply.github.com>
2025-04-30 00:30:40 +0900

    v27

--- OpenSiv3Dv0_6ForWebTest/Main.cpp
+++ OpenSiv3Dv0_6ForWebTest/Main.cpp
@@ -2,41 +2,10 @@
 #include <emscripten.h>
 #include <emscripten/val.h>
 
-Size GetViewportSize()
-{
-    int width = EM_ASM_INT({
-        return window.innerWidth;
-		});
-
-    int height = EM_ASM_INT({
-        return window.innerHeight;
-        });
-
-	return Size(width, height);
-}
-
-Size GetCanvasSize()
-{
-    int width = EM_ASM_INT({
-        // Module.canvas が存在しない場合、id="canvas" の要素を参照
-        var canvas = Module.canvas || document.getElementById('canvas');
-        return canvas ? canvas.getBoundingClientRect().width : 0;
-        });
-
-    int height = EM_ASM_INT({
-        var canvas = Module.canvas || document.getElementById('canvas');
-        return canvas ? canvas.getBoundingClientRect().height : 0;
-        });
-
-    return Size(width, height);
-}
-
 struct TouchInfo {
     int32 id;
-    double x;
-    double y;
+    Vec2 pos;
 };
-
 
 int32 getTouchesLength()
 {
@@ -46,29 +15,10 @@ int32 getTouchesLength()
 	return length;
 }
 
-// タッチ情報を取得する関数
-Array<TouchInfo> GetTouchesFromBrowser() {
-    Array<TouchInfo> result = Array<TouchInfo>(getTouchesLength());
-
-    EM_ASM({
-        const touches = siv3dActiveTouches;
-
-        for (let i = 0; i < touches.length; i++) {
-            const touch = touches[i];
-            const touchPtr = $0 + i * 24; // TouchInfo のサイズに応じて調整
-
-            setValue(touchPtr, touch.identifier, 'i32');
-            setValue(touchPtr + 8, touch.pageX, 'double');
-            setValue(touchPtr + 16, touch.pageY, 'double');
-        }
-        }, result.data());
-
-    return result;
-}
 
 // タッチ情報を取得する関数
 Array<TouchInfo> GetAdjustedTouchesFromBrowser() {
-    Array<TouchInfo> result = Array<TouchInfo>(getTouchesLength());
+    Array<TouchInfo> result = Array<TouchInfo>(EM_ASM_INT({return siv3dActiveTouches.length;}));
 
     EM_ASM({
         const touches = siv3dActiveTouches;
@@ -84,6 +34,11 @@ Array<TouchInfo> GetAdjustedTouchesFromBrowser() {
             setValue(touchPtr + 16, adjusted.y, 'double');
         }
         }, result.data());
+
+	for (auto& touch : result)
+	{
+		touch.pos = Scene::ClientToScene(touch.pos);
+	}
 
     return result;
 }
@@ -116,7 +71,17 @@ bool getPointerLock() {
 	return r != 0;
 }
 
-
+bool getSDLisDefined()
+{
+	return EM_ASM_INT({
+        if (typeof SDL != "undefined") {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+	});
+}
 
 
 
@@ -131,24 +96,8 @@ void Main()
 
 	Window::SetStyle(WindowStyle::Sizable);
 
+
 	Scene::SetResizeMode(ResizeMode::Keep);
-    /*
-    function siv3dOnTouchMove(e) {
-        siv3dActiveTouches = Array.from(e.touches);
-        // e.stopPropagation();
-    }
-
-    function siv3dOnTouchStart(e) {
-        siv3dActiveTouches = Array.from(e.touches);
-        // e.preventDefault()
-    }
-    function _siv3dRegisterTouchCallback() {
-        Module["canvas"].addEventListener("touchstart", siv3dOnTouchStart);
-        Module["canvas"].addEventListener("touchmove", siv3dOnTouchMove);
-    }
-    にならってtouchendの処理をC++側で追記
-    */
-
 	
     EM_ASM({
     Module["canvas"].addEventListener("touchend", function(e) {
@@ -160,73 +109,39 @@ void Main()
     { 
         ClearPrint();
 
+
         //Scene::Resize(GetCanvasSize());
 
-		Print << U"v26";
+		Print << U"v27";
 
         Print << U"Cursor::Pos() : " << Cursor::Pos();
-		Print << U"Cursor::ScreenPos() : " << Cursor::ScreenPos();
-		Print << U"Cursor::PosRaw() : " << Cursor::PosRaw();
 
 		Print << U"MouseL.pressed() : " << MouseL.pressed();
 		Print << U"MouseL.down() : " << MouseL.down();
 		Print << U"MouseL.up() : " << MouseL.up();
 
-		Print << U"MouseR.pressed() : " << MouseR.pressed();
-		Print << U"MouseR.down() : " << MouseR.down();
-		Print << U"MouseR.up() : " << MouseR.up();
-
-		Print << U"GetViewportSize() : " << GetViewportSize();
-
-		Print << U"GetCanvasSize() : " << GetCanvasSize();
-
-		Print << U"Scene::Size() : " << Scene::Size();
-
-		Print << U"Window::GetState().bounds : " << Window::GetState().bounds;
-
-        Print << U"Window::GetState().frameBufferSize : " << Window::GetState().frameBufferSize;
-        Print << U"Window::GetState().frameSize : " << Window::GetState().frameSize;
-
-
-
-        Print << U"Window::GetState().virtualSize : " << Window::GetState().virtualSize;
-
-        //Print << GetCanvasSize();
 		Print << U"getMousePos() : " << getMousePos();
 
         Print << U"getTouchesLength() : " << getTouchesLength();
         Print << U"getTouchesLength2() : " << getTouchesLength2();
-		Print << U"getPointerLock() : " << getPointerLock();
+		Print << U"getSDLisDefined() : " << getSDLisDefined();
 
         
 
-        const auto touches = GetTouchesFromBrowser();
-
-		for (const auto& touch : touches)
-		{
-			Print << U"Touch ID: " << touch.id << U", X: " << touch.x << U", Y: " << touch.y;
-		}
-
         const auto adjTouches = GetAdjustedTouchesFromBrowser();
-		Print << U"Adjusted Touches:";
         for (const auto& touch : adjTouches)
         {
-            Print << U"Touch ID: " << touch.id << U", X: " << touch.x << U", Y: " << touch.y;
+            Print << U"Touch ID: " << touch.id << U", pos: " << touch.pos;
         }
 
         for (const auto& touch : adjTouches) {
-            Vec2 pos = Scene::ClientToScene({ touch.x,touch.y });
-            Circle(pos, 15).drawFrame(1, Palette::Blue);
+            Circle(touch.pos, 15).drawFrame(1, Palette::Blue);
         }
 
+		Circle(Cursor::Pos(), 10).drawFrame(1,Palette::Red);
 
         Scene::Rect().drawFrame(3, 0, Palette::Blue);
         Scene::Rect().drawFrame(0, 3, Palette::Red);
-
-
-
-
-		Circle(Cursor::Pos(), 10).drawFrame(1,Palette::Red);
 
 		//SimpleGUI::TextArea(state, { 300,100 });
 
